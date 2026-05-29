793d3686ca589a96b3815453767021d82d2516bb
sansonecreig <sansonecreighoao261g@outlook.de>
2026-05-29 13:32:12 +0800

    fix: Add missing method declarations to header

--- include/DeviceSpoofer.h
+++ include/DeviceSpoofer.h
@@ -5,4 +5,13 @@
 - (NSDictionary *)deviceInfoForModel:(NSString *)model;
 - (NSArray<NSString *> *)allSupportedModels;
 - (void)applySpoofingWithModel:(NSString *)model completion:(void(^)(BOOL needsReboot))completion;
+- (void)resetKeychainShadowDomain;
+- (NSString *)fakeMachine;
+- (NSString *)fakeProductType;
+- (NSString *)fakeBoardID;
+- (uint64_t)fakeMemory;
+- (NSInteger)fakeCPUCount;
+- (NSString *)fakeSerialNumber;
+- (NSString *)fakeMLBSerial;
+- (NSString *)fakeHardwareUUID;
 @end
